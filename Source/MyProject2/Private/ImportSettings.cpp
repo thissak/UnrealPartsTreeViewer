144b2ae72c2ac63c1028db77ab4f187fb49ba6bf
qart <thissak@gmail.com>
2025-03-11 05:29:44 +0900

    - importSettings 작성중

---
+++ Source/MyProject2/Private/ImportSettings.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ImportSettings.h"
+
+
+ImportSettings::ImportSettings()
+{
+}
+
+ImportSettings::~ImportSettings()
+{
+}
---
+++ Source/MyProject2/Public/UI/ImportSettingsDialog.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MYPROJECT2_API ImportSettingsDialog
+{
+public:
+	ImportSettingsDialog();
+	~ImportSettingsDialog();
+};
